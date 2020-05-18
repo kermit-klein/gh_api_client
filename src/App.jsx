ef165bdc0b00bdad68e383b549ee16e8f3b3d382
allezz <aerbay@gmail.com>
2020-05-18 10:46:03 +0200

    Add search function,Add table render to GHsearch,Add state and move props

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
-import React from 'react'
-import Header from './components/Title'
-import GHSearch from './components/GHSearch'
-import { Container } from 'semantic-ui-react'
+import React, { Component } from "react";
+import Header from "./components/Title";
+import GHSearch from "./components/GHSearch";
+import { Container } from "semantic-ui-react";
 
-const App = () => {
-  return (
-    <Container>
-      <section name="title">
-        <Header />
-      </section>
-      <section name="main">
-        <GHSearch/>
-      </section>
-    </Container>
-  )
+export class App extends Component {
+  state = {
+    searchResult: [],
+    message: "",
+  };
+
+  searchReq = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.get("https://api.github.com/search/users", {
+        params: { q: e.target.children.search.value },
+      });
+      this.setState({ searchResult: response.data.items });
+    } catch (error) {
+      let errorMessage = error.response.data.error_message || error.message;
+      this.setState({ message: errorMessage });
+    }
+  };
+
+  render() {
+    return (
+      <Container>
+        <section name="title">
+          <Header />
+        </section>
+        <section name="main">
+          <GHSearch
+            searchReq={this.searchReq}
+            searchResult={this.state.searchResult}
+          />
+        </section>
+      </Container>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,46 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React from "react";
+import { Button, Input, List } from "semantic-ui-react";
 
 const GHSearch = () => {
+  let resultTable;
+
+  resultTable = this.props.searchResult.map((result) => {
+    let imgPath = result.avatar_url;
+
+    return (
+      <>
+        <List.Item
+          id={"result-item-" + (1 + this.props.searchResult.indexOf(result))}
+        >
+          <List.Icon name="marker" />
+          <List.Content>
+            <List.Header>{result.login}</List.Header>
+            <List.Description>
+              <a href={result.html_url}>{result.html.url}</a>
+              <img src={imgPath} height="35" alt={result.login} />
+            </List.Description>
+          </List.Content>
+        </List.Item>
+      </>
+    );
+  });
+
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
+      <form onSubmit={this.props.searchReq}>
+        <Input
+          type="text"
+          id="search"
+          name="searchText"
+          placeholder="Input GH username"
+        />
+        <Button type="submit" name="search">
+          Search
+        </Button>
+      </form>
+      <List>{resultTable}</List>
     </>
-  )
-}
+  );
+};
 
-export default GHSearch
+export default GHSearch;
